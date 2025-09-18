bed3af0b9ab5759fea72a91082b4deba290cb631
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-09-18 16:08:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
- 
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+document.querySelectorAll(".btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("stop")) {
+      stopSounds();
+    } else {
+      playSound(btn.innerText);
+    }
+  });
+});
+
+function playSound(sound) {
+  stopSounds();
+  const audio = document.getElementById(sound);
+  if (audio) {
+    audio.play();
+  }
+}
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
